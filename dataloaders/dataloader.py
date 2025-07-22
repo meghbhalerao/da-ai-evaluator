d930f437a5d116de4fa7ae58db3921329844349d
Megh Bhalerao <megh.bhalerao@gmail.com>
2025-07-21 22:43:46 -0500

    debug trajectory processing code, i.e. include reward term too in the processing

--- dataloaders/dataloader.py
+++ dataloaders/dataloader.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 import pickle
 import os
+import sys
 from packages.imitation.src.imitation.data.types import TrajectoryWithRew
 
 def convert_data_format(data_path):
@@ -10,10 +11,13 @@ def convert_data_format(data_path):
         if traj_file.endswith('.pkl'):
             with open(os.path.join(data_path, traj_file), 'rb') as f:
                 trajectory = pickle.load(f)
-                states, actions, obs = trajectory['game_states'], trajectory['actions'], trajectory['image_obs']
+                states, actions, obs, rewards = trajectory['game_states'], trajectory['actions'], trajectory['image_obs'], trajectory['rewards']
                 infos = [{"state": state} for state in states]
                 # making the saved trajectory compatible with the imitaiton learning package that we are using (I have assumed that I trust the IL package which I am using: https://imitation.readthedocs.io/en/latest/algorithms/bc.html, from berkeley, so might be reliable)
-                trajectory = TrajectoryWithRew(obs = obs, acts = actions, infos = infos, terminal = True)
+                trajectory = TrajectoryWithRew(obs = obs, acts = actions, infos = infos, rews = rewards,
+                 terminal = True)
+                print(trajectory)
+                sys.exit()
                 all_trajectories.append(trajectory)  
     return all_trajectories
     
